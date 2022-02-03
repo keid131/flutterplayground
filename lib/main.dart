da9442bf04b412cf4350b6d7e0f7e2be1c9b95a0
keid131 <keid951022@gmail.com>
2022-02-04 00:07:47 +0900

    位置情報を扱う

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +25,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,50 +33,98 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Position? _location;
+  Position? _otherLocation;
+  String _distance = '';
 
-  void _incrementCounter() {
+  Future<void> checkPermissinon() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+  }
+
+  Future<void> getLocation() async {
+    await checkPermissinon();
+    // 現在の位置を返す
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // // 北緯がプラス。南緯がマイナス
+    // print("緯度: " + position.latitude.toString());
+    // // 東経がプラス、西経がマイナス
+    // print("経度: " + position.longitude.toString());
+    // // 高度
+    // print("高度: " + position.altitude.toString());
+    // 距離をメートルで返す
+    double distanceInMeters =
+        Geolocator.distanceBetween(35.68, 139.76, -23.61, -46.40);
+    print(distanceInMeters);
+    // 方位を返す
+    double bearing = Geolocator.bearingBetween(35.68, 139.76, -23.61, -46.40);
+    print(bearing);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _location = position;
     });
   }
 
+  Future<void> getOtherLocation() async {
+    await checkPermissinon();
+    // 現在の位置を返す
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _otherLocation = position;
+    });
+    getDistance();
+  }
+
+  Future<void> getDistance() async {
+    final myLoc = _location;
+    final otherLoc = _otherLocation;
+    if (myLoc == null || otherLoc == null) {
+      return;
+    }
+
+    double distanceInMeters = Geolocator.distanceBetween(
+        myLoc.latitude, myLoc.longitude, otherLoc.latitude, otherLoc.longitude);
+    print(distanceInMeters);
+    // 方位を返す
+    double bearing = Geolocator.bearingBetween(
+        myLoc.latitude, myLoc.longitude, otherLoc.latitude, otherLoc.longitude);
+    print(bearing);
+    setState(() {
+      _distance = distanceInMeters.toString();
+    });
+  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -102,14 +134,29 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            const Text(
+              '現在の位置は:',
+            ),
+            Text(
+              _location.toString(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            const Text(
+              '距離は:',
+            ),
+            Text(
+              _distance,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            TextButton(onPressed: getOtherLocation, child: const Text("計算"))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: getLocation,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
